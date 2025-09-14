edfc6f8d58745e3e5b892bb547ad6f8b82702fb9
SoniAryanXmart <154493242+Githubsoniaryan@users.noreply.github.com>
2025-09-14 17:00:20 +0530

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -28,7 +28,7 @@ async def start(event):
     if not event.is_private:
         return await event.respond("ɪ ᴀᴍ ᴀʟɪᴠᴇ 🥺")
     await event.reply(
-     "━━━━━━━━━━━━━━━━━━━\n👇 𝗦𝘁𝗮𝗿𝘁 𝗧𝗵𝗶𝘀 𝗕𝗼𝘁 𝗙𝗮𝘀𝘁 👇\n\n➜ @PujaMusicRobot 💋\n➜ @PujaMusicRobot 💋\n➜ @PujaMusicRobot 💋\n➜ @PujaMusicRobot 💋\n\n👆 𝗦𝘁𝗮𝗿𝘁 𝗧𝗵𝗶𝘀 𝗕𝗼𝘁 𝗙𝗮𝘀𝘁 👆\n━━━━━━━━━━━━━━━━━━━",
+     "━━━━━━━━━━━━━━━━━━━\n👇 𝗦𝘁𝗮𝗿𝘁 𝗧𝗵𝗶𝘀 𝗕𝗼𝘁 𝗙𝗮𝘀𝘁 👇\n\n➜ @PujaMusicRobot 💋\n➜ @FloraMusiccBot 💋\n➜ @BellaMusiccBot 💋\n➜ @PujaChatMusicRobot 💋\n\n👆 𝗦𝘁𝗮𝗿𝘁 𝗧𝗵𝗶𝘀 𝗕𝗼𝘁 𝗙𝗮𝘀𝘁 👆\n━━━━━━━━━━━━━━━━━━━",
         link_preview=False,
         buttons=(
             [
